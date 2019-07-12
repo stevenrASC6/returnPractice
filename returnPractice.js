da66ad406d25aff5d50487f2cb53084e5944a353
stevenrASC6 <steven312002@gmail.com>
2019-07-12 09:46:43 -0400

    Creates function with return value

---
+++ returnPractice.js
@@ -0,0 +1,9 @@
+function someFunc(){
+//return syntax
+    return "Hello World";
+    //return 5;
+}
+
+const result = someFunc();
+
+console.log (result);   // By default all javascript functions return a value. If it's not specified it returns udnefined
